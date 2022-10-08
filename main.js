24a2eff7f3ee3905992812302a9da051adb0ea2f
Mohinur <hikmatovamohinur2007@gmail.com>
2022-10-08 16:22:54 +0500

    hello

---
+++ main.js
@@ -0,0 +1,182 @@
+let a = 10
+let b = 10
+console.log(10==10)
+// console.log(10===10)
+//  console.log("10"==10)
+// console.log('10'===10)
+// console.log(4/0)
+// console.log(0/4)
+// console.log(-4/0)
+// console.log(0/0)
+// console.log(1/1)
+
+// console.log(4+4)
+// console.log('4'-5)
+
+
+
+// let x =5
+// x = x+1
+// x+=1
+// x++
+// console.log(x++)
+
+
+//  alert(5)
+//  confirm('Islom ukami')
+
+// prompt('What is you name?')
+
+
+
+
+
+// for (let i=0; i<10; i++){
+//     console.log(i)
+// }
+
+// let i=0;
+// do{
+//     console.log(i)
+//     i++
+// }while(i<10)
+
+// let i=0
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+
+
+
+
+//  for (let i=0; i<10; i--){
+//      console.log(i)
+//  }
+
+
+// for (let i=0;  i>10; i++){
+//     console.log(i)
+// }
+
+// for (let i=10; i<0; i++){
+//     console.log(i)
+// }
+
+//  for (let i=0; i<26; i++){
+//      console.log(i)
+//  }
+
+
+// let answers= []
+// let questions =[
+//         'o Islom kimsan',
+//         'nechinchi sinf san',
+//         'yoshing nechi'
+// ]
+// for (let i =0; i<questions.length; i++){
+//     answers[i]=prompt(questions[i])
+// }
+// console.log(answers);
+
+
+//  let answers= []
+//  let questions =[
+//     'kim san',
+//     'kimligingni ayt',
+//     'yoshing nechi'
+//  ]
+//  for (let i =0, i<questions.length; i++){
+//     answers[i]=prompt(questions[i])
+//  }
+//  console.log(answers)
+
+
+
+
+
+
+//             IF else& switch case
+// let age = 15
+// if (age<18){
+//     alert('hush kelibsiz')
+// }else{
+//     alert('yoq bol')
+// }
+
+
+
+
+
+// let age = prompt('yoshing nechida')
+//     if (age<18) {
+//         alert('holi vaqtli')
+// }else if(age<80){
+//     alert('hush kelibsiz')
+// }else{
+//     alert('uje kech')
+// }
+
+
+
+
+// let age = prompt('Yoshing nechi')
+// switch (age) {
+//     case '18':
+//         alert('yoshing 18da')
+//         break;
+//     case '50':
+//         alert('Yoshing 50da')
+//         break
+//     default:
+//         alert('yoshing yoq')
+//         break;
+// }
+
+
+
+
+
+
+
+
+// let age = prompt('Yoshing nechi')
+// switch (age) {
+//     case '0':
+//         alert('yoshing yoq')
+//         break;
+//     case '10':    
+//         alert('yoshing 10da')
+//         break;
+//     case '20':
+//         alert('yoshing 20da')
+//         break;
+//     case '30':
+//         alert('yoshing 30da')
+//         break;
+//     case '40':
+//         alert('yoshing 40da')
+//         break;
+//     case '50':
+//         alert('yoshing 50da')
+//         break;
+//     case '60':
+//         alert('yoshing 60da')
+//         break;
+//     case '70':
+//         alert('yoshing 70da')
+//         break;
+//     case '80':
+//         alert('yoshing 80da')
+//         break; 
+//     case '90':
+//         alert('yoshing 90da')
+//         break;
+//     case '100':
+//         alert('yoshing 100da')
+//         break;       
+//     default:
+//         alert('yoshing yoq')
+//         break;
+// }
